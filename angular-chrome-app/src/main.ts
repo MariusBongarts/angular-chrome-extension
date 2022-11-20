737b926a0f7395dc6c0c75e288812859cdd55ba6
MariusBongarts <marius.bongarts@external.sinnerschrader.com>
2022-11-20 11:19:29 +0100

    Setup build scripts

--- angular-chrome-app/src/main.ts
+++ angular-chrome-app/src/main.ts
@@ -9,20 +9,6 @@ let rootElement = document.querySelector(ROOT_ELEMENT_TAG);
 if (!rootElement) {
   rootElement = document.createElement(ROOT_ELEMENT_TAG);
   rootElement.id = 'angular-chrome-app';
-  const globalStyles = document.createElement('style');
-  globalStyles.innerHTML = `
-    ${ROOT_ELEMENT_TAG} {
-      position: fixed;
-      left: 0;
-      top: 0;
-      width: 300px;
-      height: 100vh;
-      background: #ffffff;
-      border-right: 1px solid #c2c2c2;
-      z-index: 999;
-      }
-  `;
-  document.body.appendChild(globalStyles);
   document.body.appendChild(document.createElement(ROOT_ELEMENT_TAG));
 }
 
